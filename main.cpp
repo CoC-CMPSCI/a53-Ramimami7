0c3fa1f3de0b63c2bb4b03a36e0d7794efd1f0ff
Ramimami7 <rhabbal22@gamil.com>
2026-03-31 22:32:25 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,13 @@ int main()
 {
     int i, begin, end, sum = 0;
 
-    
     cout << "Enter begin and end values: ";
-    cin >> begin >> end;
-
-    
-    while (begin > end) {
+    while (!(cin >> begin >> end) || begin > end) {
+        cin.clear();
+        cin.ignore(1000, '\n');
         cout << "Enter begin and end values: ";
-        cin >> begin >> end;
     }
 
-    
     for (i = begin; i <= end; i++) {
         if (i % 2 == 0) {
             sum += i;
