532d7e681e25e6d636409667e7b4070b5893f921
Ramimami7 <rhabbal22@gamil.com>
2026-03-31 22:36:59 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     int i, begin, end, sum = 0;
 
-    cout << "Enter begin and end values: ";
-    while (!(cin >> begin >> end) || begin > end) {
-        cin.clear();
-        cin.ignore(1000, '\n');
+    
+    do {
         cout << "Enter begin and end values: ";
-    }
+        cin >> begin >> end;
+    } while (begin > end);
 
+    
     for (i = begin; i <= end; i++) {
         if (i % 2 == 0) {
             sum += i;
@@ -20,6 +20,4 @@ int main()
     }
 
     cout << "The sum of even values is " << sum << endl;
-
-    return 0;
 }
